feb7f6f8f0b20d5bde99b6f3a19b9f1aaf5800b7
Kyle Rothenbaum <krothenbaum@gmail.com>
2017-04-11 16:00:13 -0700

    UI: Register Page

--- public/js/draft-tracker.js
+++ public/js/draft-tracker.js
@@ -3,15 +3,7 @@ $(document).ready(function () {
 		section: '.scroll-section',
 		scrollbars: false,
 		before: function(index,panel) {
-			console.log(index);
-			console.log(panel);
-			console.log(panel[index])
-			// $('.scroll-section img').removeClass('slideLefToRight');
-			// $('.scroll-section img').addClass('slideLefToRight');
 			$(panel[index]).find('img').addClass('slideLefToRight');
 		},
-		// after: function() {
-		// 	$('.scroll-section img').addClass('.fadeIn');
-		// },
 	});
 })
