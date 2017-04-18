584748e7f6c91a89367784ccf1e84ca17fbff3de
Kyle Rothenbaum <krothenbaum@gmail.com>
2017-04-17 18:22:55 -0700

    Delete and Edit endpoints completed

--- routes/index.js
+++ routes/index.js
@@ -106,12 +106,12 @@ router.post('/user/add/draft', (req, res) => {
 //delete by draft id
 router.post('/user/draft/delete', (req, res) => {
   User
-    .findById(req.body.userid)
+    .findById(req.user.id)
     .exec()
     .then(user => {
       user.drafts.pull(req.body.draftid);
       user.save();
-      res.status(204).redirect('/dashboard');
+      res.status(202).redirect('/dashboard');
     })
     .catch(err => {
       console.error(err);
@@ -119,52 +119,51 @@ router.post('/user/draft/delete', (req, res) => {
     });
 });
 
-//save edited draft
-router.post('/user/edit/draft', (req, res) => {
-  // console.log(req.draftid)
-  // let draft = "{drafts._id: ObjectId("+req.draftid+")}";
-  // let draft2 = ()
-  console.log(req.user.id);
-  console.log(req.body.draftid);
+//update a draft record
+router.post('/user/edit/update', (req, res) => {
+  const requiredFields = ['date','sets', 'format', 'colorsPlayed','matches'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  for(let i=0; i < req.body.matches.length; i++) {
+    const gamesWonInt = parseInt(req.body.matches[i].gamesWon);
+    const gamesLostInt = parseInt(req.body.matches[i].gamesLost);
+    if (gamesLostInt < gamesWonInt) {
+      req.body.matches[i].matchWon = true;
+    }
+    else {
+      req.body.matches[i].matchWon = false;
+    }
+  }
 
   User
-  // .find({drafts: {$elemMatch: {_id: req.body.draftid}}})
-   .update({"_id" : req.user.id, "drafts._id" : req.body.draftid},{$set : {"drafts.$.sets" : 'fjsdklfjs'}})
-   // .find({'_id': req.user.id}, {'drafts': {'$elemMatch': {'_id': req.body.draftid}}})
-   .exec()
-   .then(draft => {
-      
-      console.log(draft);
-      console.log(draft.drafts);
-      // render('edit-draft', { title: 'Add Draft', draft : req.draft })
-   })
+    .findById(req.user.id)
+    .exec()
+    .then(user => {
+      user.drafts.pull(req.body.draftId);
+      user.drafts.push(req.body);
+      user.save();
+      res.status(201).redirect('/dashboard');
+    })
    .catch(err => {
     console.error(err);
     res.status(500).redirect('/dashboard');
-   })
+   });
 });
 
-router.post('/edit-draft', (req, res) => {
-  //query for draft
-  User
-    .find({drafts: {$elemMatch: {_id: req.body.draftid}}}, {'drafts.$._id': 1})
-    .exec()
-    .then(draft => {
-      console.dir(draft);
-      // console.log(draft.drafts._id);
-      // console.log(draft.drafts.matches);
-      res.render('edit-draft', { title: 'Edit Draft', user : req.user, draft: req.body.draftid });
-    });
-  
-});
-
-//update draft record
+//get draft to edit and pass draft to edit draft page
 router.post('/user/draft/edit', (req, res) => {
   User
-    .findById(req.body.userid)
+    .find({_id : req.user.id},{drafts:{ $elemMatch: {id:req.body.draftId} }})
     .exec()
-    .then(user => {
-      //find draft and update
+    .then(results => {
+      res.render('edit-draft', {title: 'Edit Draft', user: req.user, draft: results[0].drafts[0]});
     })
     .catch(err => {
       console.error(err);
